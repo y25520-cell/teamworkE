8e7b42a8e82dc486dcdf3b7388aba90969730c2c
y25520-cell <y25520@iwate-it.ac.jp>
2026-02-16 14:33:02 +0900

    プログラム追加
    プログラム簡単に追加しました。

--- system.js
+++ system.js
@@ -7,7 +7,14 @@ const foods = [
     { name: "怪しいキノコ", risk: 40, heal: 35, image: "mushroom.png" },
     { name: "腐った肉", risk: 65, heal: 50, image: "meat.png" },
     { name: "水", risk: 1, heal: 5, image: "water.png" },
-    { name: "謎の薬", risk: 50, heal: 60, image: "drug.png" }
+    { name: "玉子豆腐", risk: 99, heal: 100, image: "drug.png" },
+    { name: "消しゴム", risk: 10, heal: 10, image: "drug.png" },
+    { name: "鉛筆の芯", risk: 20, heal: 5, image: "drug.png" },
+    { name: "鼻セレブ", risk: 5, heal: 10, image: "drug.png" },
+    { name: "灯油", risk: 70, heal: 30, image: "drug.png" },
+    { name: "輪ゴム", risk: 20, heal: 10, image: "drug.png" },
+    { name: "化粧水", risk: 30, heal: 30, image: "drug.png" },
+    { name: "たくまの腕", risk: 70, heal: 70, image: "takuma.png" }
 ];
 
 
